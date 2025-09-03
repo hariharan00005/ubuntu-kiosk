09055d4c08d64e1ada75e593e557454c82d6e161
hariharan00005 <hariharan.swaminathan@softsuave.org>
2025-09-03 22:21:00 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </Provider>
 );
